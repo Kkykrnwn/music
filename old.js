3541f95232678463560303eb8bdadb51ab7882bc
Kkykrnwn <161400890+Kkykrnwn@users.noreply.github.com>
2025-09-09 11:19:46 +0700

    Add files via upload

---
+++ old.js
@@ -0,0 +1,128 @@
+// playlist_old.js
+const oldPlaylist = [
+  {
+    name: "All About That Bass",
+    src: "old/all about that bass.mp3",
+    cover: "https://picsum.photos/200/200?random=4",
+  },
+  {
+    name: "Awing Mawek",
+    src: "old/awing mawek.mp3",
+    cover: "https://picsum.photos/200/200?random=5",
+  },
+  {
+    name: "Believer",
+    src: "old/believer.mp3",
+    cover: "https://picsum.photos/200/200?random=4",
+  },
+  {
+    name: "Despacito",
+    src: "old/despacito.mp3",
+    cover: "https://picsum.photos/200/200?random=5",
+  },
+  {
+    name: "Dj Aku Suka Body Aisyah",
+    src: "old/dj aku suka body aisyah.mp3",
+    cover: "https://picsum.photos/200/200?random=4",
+  },
+  {
+    name: "Dj Bermain Dubstep",
+    src: "old/dj bermain dubstep.mp3",
+    cover: "https://picsum.photos/200/200?random=5",
+  },
+  {
+    name: "Dj Don't Watch Me Cry",
+    src: "old/dj don't watch me cry.mp3",
+    cover: "https://picsum.photos/200/200?random=4",
+  },
+  {
+    name: "Dj Emang Mantul Punya Istri Dua",
+    src: "old/dj emang mantul punya istri dua.mp3",
+    cover: "https://picsum.photos/200/200?random=5",
+  },
+  {
+    name: "Dj Entah Apa Yang Merasukimu",
+    src: "old/dj entah apa yang merasukimu.mp3",
+    cover: "https://picsum.photos/200/200?random=4",
+  },
+  {
+    name: "Dj Everything At Once",
+    src: "old/dj everything at once.mp3",
+    cover: "https://picsum.photos/200/200?random=5",
+  },
+  {
+    name: "Dj Haruskah Berakhir",
+    src: "old/dj haruskah berakhir.mp3",
+    cover: "https://picsum.photos/200/200?random=4",
+  },
+  {
+    name: "Dj Harusnya Aku",
+    src: "old/dj harusnya aku.mp3",
+    cover: "https://picsum.photos/200/200?random=5",
+  },
+  {
+    name: "Dj Kusimpan Rindu Di hati",
+    src: "old/dj kusimpan rindu di hati.mp3",
+    cover: "https://picsum.photos/200/200?random=4",
+  },
+  {
+    name: "Dj Mungkin Sang Fajar",
+    src: "old/dj mungkin sang fajar.mp3",
+    cover: "https://picsum.photos/200/200?random=5",
+  },
+  {
+    name: "Dj Pong Pong",
+    src: "old/dj pong pong.mp3",
+    cover: "https://picsum.photos/200/200?random=4",
+  },
+  {
+    name: "Dj Satu Hati Sampai Mati",
+    src: "old/dj satu hati sampai mati.mp3",
+    cover: "https://picsum.photos/200/200?random=5",
+  },
+  {
+    name: "Dj Simphony Kaweni",
+    src: "old/dj simphony kaweni.mp3",
+    cover: "https://picsum.photos/200/200?random=4",
+  },
+  {
+    name: "Dj Slow Make It Bun Dem",
+    src: "old/dj slow make it bun dem.mp3",
+    cover: "https://picsum.photos/200/200?random=5",
+  },
+  {
+    name: "Dj Sungguh Ku Merasa Resah",
+    src: "old/dj sungguh ku merasa resah.mp3",
+    cover: "https://picsum.photos/200/200?random=4",
+  },
+  {
+    name: "Dj Trouble Is A Friends",
+    src: "old/dj trouble is a friends.mp3",
+    cover: "https://picsum.photos/200/200?random=5",
+  },
+  {
+    name: "Dj Walaupun Terbentang Jarak",
+    src: "old/dj walaupun terbentang jarak.mp3",
+    cover: "https://picsum.photos/200/200?random=5",
+  },
+  {
+    name: "Don't Let Me Down",
+    src: "old/don't let me down.mp3",
+    cover: "https://picsum.photos/200/200?random=4",
+  },
+  {
+    name: "Girls Like You",
+    src: "old/girls like you.mp3",
+    cover: "https://picsum.photos/200/200?random=5",
+  },
+  {
+    name: "Payphone",
+    src: "old/payphone.mp3",
+    cover: "https://picsum.photos/200/200?random=4",
+  },
+  {
+    name: "Waka Waka",
+    src: "old/waka waka.mp3",
+    cover: "https://picsum.photos/200/200?random=5",
+  },
+];
